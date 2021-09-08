696bdfc1ea002b74db1f01bcbc1781d1f3cc1aad
PaulHodgson <paul.hodgson073@gmail.com>
2021-09-08 16:35:00 +0100

    [NONPR-2224] add formatter; minor scala bump; format code and remove warnings

--- src/main/scala/uk/gov/hmrc/nonrep/attachment/server/NonrepMicroservice.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/server/NonrepMicroservice.scala
@@ -5,6 +5,7 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 
+import scala.concurrent.Future
 import scala.util.{Failure, Success}
 
 object NonrepMicroservice {
@@ -14,11 +15,16 @@ object NonrepMicroservice {
 class NonrepMicroservice(routes: Routes)(implicit val system: ActorSystem[_], config: ServiceConfig) {
   import system.executionContext
 
-  val serverBinding = Http().newServerAt("0.0.0.0", config.port).bind(routes.serviceRoutes)
+  val serverBinding: Future[Http.ServerBinding] = Http().newServerAt("0.0.0.0", config.port).bind(routes.serviceRoutes)
   serverBinding.onComplete {
     case Success(binding) =>
       val address = binding.localAddress
-      system.log.info("Server '{}' is online at http://{}:{}/ with configuration: {}", config.appName, address.getHostString, address.getPort, config.toString)
+      system.log.info(
+        "Server '{}' is online at http://{}:{}/ with configuration: {}",
+        config.appName,
+        address.getHostString,
+        address.getPort,
+        config.toString)
     case Failure(ex) =>
       system.log.error("Failed to bind HTTP endpoint, terminating system", ex)
       system.terminate()
@@ -26,24 +32,23 @@ class NonrepMicroservice(routes: Routes)(implicit val system: ActorSystem[_], co
 }
 
 object Main {
+
   /**
-   * https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/java-dg-jvm-ttl.html
-   */
+    * https://docs.aws.amazon.com/sdk-for-java/v1/developer-guide/java-dg-jvm-ttl.html
+    */
   java.security.Security.setProperty("networkaddress.cache.ttl", "60")
 
   implicit val config: ServiceConfig = new ServiceConfig()
 
   def main(args: Array[String]): Unit = {
     val rootBehavior = Behaviors.setup[Nothing] { context =>
-
-
       val routes = Routes()(context.system, implicitly)
 
       NonrepMicroservice(routes)(context.system, config)
 
       Behaviors.empty
     }
-    implicit val system = ActorSystem[Nothing](rootBehavior, s"NrsServer-${config.appName}")
 
+    implicit val system: ActorSystem[Nothing] = ActorSystem[Nothing](rootBehavior, s"NrsServer-${config.appName}")
   }
 }
