10773b37137d048ffb0f2034a4845a326d12cdf4
Paul Hodgson <Paul.hodgson@equalexperts.com>
2021-11-23 10:18:56 +0000

    [NONPR-1992] add implicit and explicit auditing

--- src/main/scala/uk/gov/hmrc/nonrep/attachment/models/models.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/models/models.scala
@@ -1,17 +1,16 @@
 package uk.gov.hmrc.nonrep.attachment.models
 
+import io.prometheus.client.Histogram.Timer
 import spray.json.{JsObject, JsValue}
 import uk.gov.hmrc.nonrep.attachment.{ApiKey, Id}
 
-case class AttachmentRequest(attachmentUrl: Id,
-                             attachmentId: Id,
-                             payloadSha256Checksum: Id,
-                             attachmentContentType: Id,
-                             nrSubmissionId: Id)
+case class AttachmentRequest(attachmentUrl: Id, attachmentId: Id, payloadSha256Checksum: Id, attachmentContentType: Id, nrSubmissionId: Id)
 
 case class IncomingRequest(apiKey: ApiKey, request: JsValue)
 
-case class AttachmentRequestKey(apiKey: ApiKey, request: AttachmentRequest)
+case class AttachmentRequestKey(apiKey: ApiKey, request: AttachmentRequest, maybeTimer: Option[Timer] = None) {
+  def withTimer(timer: Timer): AttachmentRequestKey = AttachmentRequestKey(apiKey, request, Some(timer))
+}
 
 case class AttachmentResponse(attachmentId: Id)
 
--- src/main/scala/uk/gov/hmrc/nonrep/attachment/server/Routes.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/server/Routes.scala
@@ -2,9 +2,9 @@ package uk.gov.hmrc.nonrep.attachment
 package server
 
 import akka.actor.typed.ActorSystem
-import akka.http.scaladsl.common.EntityStreamingSupport
+import akka.http.scaladsl.common.{EntityStreamingSupport, JsonEntityStreamingSupport}
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
-import akka.http.scaladsl.model.StatusCodes.{Accepted, BadRequest, InternalServerError, OK}
+import akka.http.scaladsl.model.StatusCodes.{Accepted, InternalServerError, OK}
 import akka.http.scaladsl.model.{HttpEntity, HttpResponse}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.directives.MethodDirectives.{get, post}
@@ -15,13 +15,13 @@ import akka.stream.Attributes.logLevels
 import akka.stream.scaladsl.{Keep, Sink}
 import fr.davit.akka.http.metrics.core.scaladsl.server.HttpMetricsDirectives.{pathLabeled, _}
 import fr.davit.akka.http.metrics.prometheus.marshalling.PrometheusMarshallers._
+import spray.json._
 import uk.gov.hmrc.nonrep.BuildInfo
 import uk.gov.hmrc.nonrep.attachment.metrics.Prometheus._
-import uk.gov.hmrc.nonrep.attachment.models.{AttachmentRequest, AttachmentRequestKey, AttachmentResponse, IncomingRequest}
+import uk.gov.hmrc.nonrep.attachment.models.{AttachmentResponse, IncomingRequest}
+import uk.gov.hmrc.nonrep.attachment.service.ResponseService
 import uk.gov.hmrc.nonrep.attachment.stream.AttachmentFlow
 import uk.gov.hmrc.nonrep.attachment.utils.JsonFormats._
-import uk.gov.hmrc.nonrep.attachment.service.ResponseService
-import spray.json._
 
 import scala.util.{Failure, Success}
 
@@ -33,7 +33,7 @@ class Routes(flow: AttachmentFlow)(implicit val system: ActorSystem[_], config: 
 
   import ResponseService.ops._
 
-  implicit val jsonStreamingSupport = EntityStreamingSupport.json()
+  private implicit val jsonStreamingSupport: JsonEntityStreamingSupport = EntityStreamingSupport.json()
   private val headerApiKey = "x-api-key"
 
   private val log = system.log
@@ -52,6 +52,8 @@ class Routes(flow: AttachmentFlow)(implicit val system: ActorSystem[_], config: 
               headerValueByName(headerApiKey) { apiKey =>
 
                 entity(asSourceOf[JsValue]) { request =>
+                  val attachmentResponseTimer = attachmentResponseTimesHistogram.startTimer()
+
                   val stream = request
                     .log(name = "attachmentFlow")
                     .addAttributes(logLevels(onElement = Off, onFinish = Info, onFailure = Error))
@@ -60,23 +62,32 @@ class Routes(flow: AttachmentFlow)(implicit val system: ActorSystem[_], config: 
                     .toMat(Sink.head)(Keep.right)
                     .run()
 
-                  onComplete(stream) {
-                    case Success(result) =>
-                      result.fold[StandardRoute](
-                        err => {
-                          log.error("Attachment service error {}", err)
-                          err.completeAsJson()
-                        },
-                        res => {
-                          complete {
-                            HttpResponse(Accepted, entity = HttpEntity(AttachmentResponse(res.attachmentId).toJson.toString))
+                  onComplete(stream) { completion =>
+                    val response = completion match {
+                      case Success(result) =>
+                        result.fold[StandardRoute](
+                          err => {
+                            log.error("Attachment service error {}", err)
+                            attachmentFailureCounter.labels(err.code.intValue.toString).inc()
+                            err.completeAsJson()
+                          },
+                          res => {
+                            complete {
+                              attachmentSuccessCounter.inc()
+                              HttpResponse(Accepted, entity = HttpEntity(AttachmentResponse(res.attachmentId).toJson.toString))
+                            }
                           }
-                        }
-                      )
-                    case Failure(x) =>
-                      val message = "Internal NRS error"
-                      log.error(s"$message, caused by ${x.getCause}", x)
-                      ErrorMessage(message, InternalServerError).completeAsJson()
+                        )
+                      case Failure(x) =>
+                        val message = "Internal NRS error"
+                        log.error(s"$message, caused by ${x.getCause}", x)
+                        attachmentFailureCounter.labels(InternalServerError.intValue.toString).inc()
+                        ErrorMessage(message, InternalServerError).completeAsJson()
+                    }
+
+                    attachmentResponseTimer.observeDuration()
+
+                    response
                   }
                 }
               }
