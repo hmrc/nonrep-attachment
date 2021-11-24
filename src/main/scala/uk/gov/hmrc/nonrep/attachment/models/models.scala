82b630a78aba17fa9d6a9094fb0187f6710bb145
craigtd <16776616+craigtd@users.noreply.github.com>
2021-11-24 11:39:21 +0000

    Revert "[NONPR-1992] add implicit and explicit auditing"
    This reverts commit 10773b37137d048ffb0f2034a4845a326d12cdf4.

--- src/main/scala/uk/gov/hmrc/nonrep/attachment/models/models.scala
+++ src/main/scala/uk/gov/hmrc/nonrep/attachment/models/models.scala
@@ -1,16 +1,17 @@
 package uk.gov.hmrc.nonrep.attachment.models
 
-import io.prometheus.client.Histogram.Timer
 import spray.json.{JsObject, JsValue}
 import uk.gov.hmrc.nonrep.attachment.{ApiKey, Id}
 
-case class AttachmentRequest(attachmentUrl: Id, attachmentId: Id, payloadSha256Checksum: Id, attachmentContentType: Id, nrSubmissionId: Id)
+case class AttachmentRequest(attachmentUrl: Id,
+                             attachmentId: Id,
+                             payloadSha256Checksum: Id,
+                             attachmentContentType: Id,
+                             nrSubmissionId: Id)
 
 case class IncomingRequest(apiKey: ApiKey, request: JsValue)
 
-case class AttachmentRequestKey(apiKey: ApiKey, request: AttachmentRequest, maybeTimer: Option[Timer] = None) {
-  def withTimer(timer: Timer): AttachmentRequestKey = AttachmentRequestKey(apiKey, request, Some(timer))
-}
+case class AttachmentRequestKey(apiKey: ApiKey, request: AttachmentRequest)
 
 case class AttachmentResponse(attachmentId: Id)
 
